9144a84932555b2a65514193f6a2d9e17956e7ad
Diana Korotkevica <dianakorotkevica@iPhone-Ilkka.giosg.lan>
2022-02-17 11:34:18 +0200

    first steps

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React, {FC} from 'react';
+import "./App.css";
+
+
+const App: FC = () => {
+  return (
+    <div className="App">
+  <div className="header">
+    <input type="text" placeholder='Task.....'/>
+    <input type="number" placeholder='Deadline (in Days)'/>
+    <button>Add Task</button>
+  </div>
+  <div className="todoList"></div>
+    </div>
+  );
+}
+
+export default App;
