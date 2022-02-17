ccf70694725250ec7a1a5e1260618980f442a914
Diana Korotkevica <dianakorotkevica@iPhone-Ilkka.giosg.lan>
2022-02-17 12:29:14 +0200

    css styles adding

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,10 @@ const App: FC = () => {
   return (
     <div className="App">
   <div className="header">
+    <div className="inputContainer">
     <input type="text" placeholder='Task.....'/>
     <input type="number" placeholder='Deadline (in Days)'/>
+    </div>
     <button>Add Task</button>
   </div>
   <div className="todoList"></div>
